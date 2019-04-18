18e19b39f8909f75f93a991e6e5ee83adc9283d0
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-04-18 15:35:36 +0800

    Issue with react-native#22175 and jest

---
+++ jest/preprocessor.js
@@ -0,0 +1,61 @@
+/**
+ * Temporary fix due to issue with react-native jest/preprocessor.js
+ * Solution:
+ * - https://github.com/facebook/react-native/issues/22175#issuecomment-477138096
+ *
+ * - https://github.com/facebook/react-native/issues/22175
+ * - https://github.com/facebook/react-native/issues/22175
+ */
+
+/**
+ * Your own [temporary?] transform for React Native
+ */
+const generate = require('@babel/generator').default
+const transformer = require('metro-react-native-babel-transformer')
+const createCacheKeyFunction = require('fbjs-scripts/jest/createCacheKeyFunction')
+const metroBabelRegister = require('metro-babel-register')
+
+metroBabelRegister([])
+
+module.exports = {
+  process(src, file) {
+    const { ast } = transformer.transform({
+      filename: file,
+      options: {
+        ast: true,
+        dev: true,
+        enableBabelRuntime: false,
+        experimentalImportSupport: false,
+        hot: false,
+        inlineRequires: false,
+        minify: false,
+        platform: '',
+        projectRoot: '',
+        retainLines: true,
+        sourceType: 'unambiguous'
+      },
+      src,
+      plugins: metroBabelRegister.config.plugins
+    })
+
+    return generate(
+      ast,
+      {
+        code: true,
+        comments: false,
+        compact: false,
+        filename: file,
+        retainLines: true,
+        sourceFileName: file,
+        sourceMaps: true
+      },
+      src
+    ).code
+  },
+
+  getCacheKey: createCacheKeyFunction([
+    __filename,
+    require.resolve('metro-react-native-babel-transformer'),
+    require.resolve('@babel/core/package.json')
+  ])
+}
