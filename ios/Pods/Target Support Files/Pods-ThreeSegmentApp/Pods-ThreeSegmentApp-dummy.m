333d289caff911adb4bdcabf1d112d09b20de96b
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-04-23 19:25:18 +0800

    Should not ignore Pod folder for appcenter

---
+++ ios/Pods/Target Support Files/Pods-ThreeSegmentApp/Pods-ThreeSegmentApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThreeSegmentApp : NSObject
+@end
+@implementation PodsDummy_Pods_ThreeSegmentApp
+@end
---
+++ ios/Pods/Target Support Files/Pods-ThreeSegmentAppTests/Pods-ThreeSegmentAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThreeSegmentAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ThreeSegmentAppTests
+@end
